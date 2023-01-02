7cb989661d84a5a9ec39d175adcd9b44eed53614
Nika Qobulashvili <117595334+QOBULA23@users.noreply.github.com>
2023-01-02 17:00:25 +0400

    Add files via upload

---
+++ spring-boot-example/spring-boot-example/src/main/java/com/qobulaspring/Main.java
@@ -0,0 +1,71 @@
+package com.qobulaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
+
+@SpringBootApplication
+@RestController
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        GreetResponse response = new GreetResponse(
+                "Hello",
+                List.of("Porsche", "BMW", "Mercedes-Benz"),
+                new Person("Nika", 22, 45_000)
+        );
+        return response;
+    }
+     record Person (String name, int age, double AvaliableCash) {
+
+     }
+     record GreetResponse(
+             String greet,
+             List<String> favCarBrands,
+             Person person
+     ){}
+
+//    class GreetResponse {
+//        private final String greet;
+//
+//
+//        GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse that = (GreetResponse) o;
+//            return Objects.equals(greet, that.greet);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(greet);
+//        }
+//    }
+
+}
+
